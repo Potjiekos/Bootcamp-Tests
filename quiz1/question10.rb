03aca2b9c40f0b0df5049c15922da97bb7c32436
Potjiekos <AshenDamas@gmail.com>
2016-03-29 11:46:56 -0700

    include vs extend

---
+++ quiz1/question10.rb
@@ -0,0 +1,1 @@
+# 10. Write a piece of code to demonstrate the difference between `include` and `extend` in using Ruby modules with classes to include methods. Make sure to demonstrate calling the methods with your code.
