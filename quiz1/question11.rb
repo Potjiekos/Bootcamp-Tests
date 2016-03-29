22b95cecfb81850256464902a1a010ecd65d59fe
Potjiekos <AshenDamas@gmail.com>
2016-03-29 13:11:17 -0700

    Q7 without mutating variable

--- quiz1/question11.rb
+++ quiz1/question11.rb
@@ -1,1 +1,18 @@
 # 11. BONUS: Rewrite the method `titleize` in question #7 without mutating any variable.
+
+# module HelperMethods
+#   def titleize(string)
+#     capword = string.split(" ").map do |x|
+#     (x == "in" || x == "the" || x == "of" || x == "or" || x == "from") ? x : x.capitalize
+#     end
+#     print capword.join(" ")
+#   end
+# end
+
+module HelperMethods
+  def titleize(string)
+    print string.split(" ").each do |x|
+      (x == "in" || x == "the" || x == "of" || x == "or" || x == "from") ? x : x.capitalize
+    end
+  end
+end
