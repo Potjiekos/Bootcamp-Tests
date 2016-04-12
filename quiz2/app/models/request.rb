e8fced4cc379be8a3d9f82742d5a91c3249c2b88
Potjiekos <AshenDamas@gmail.com>
2016-04-11 17:39:15 -0700

    stupid search works yay

--- quiz2/app/models/request.rb
+++ quiz2/app/models/request.rb
@@ -1,9 +1,8 @@
 class Request < ActiveRecord::Base
 
   def self.search(search)
-    where("name LIKE ?", "%#{search}%")
-    where("email LIKE ?", "%#{search}%")
-    where("message LIKE ?", "%#{search}%")
+
+      where("name ILIKE ? OR message ILIKE ? OR email ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
   end
 
 end
